dec3d86dd5769890f280ff3b7ec3ed53f5464188
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-09-11 11:18:29 +0200

    Make sure request stubs are called

--- test/request.js
+++ test/request.js
@@ -20,70 +20,88 @@ describe('request', function () {
     describe('get()', function () {
         it('should set the request method to "GET"', function () {
             var request = new Request({});
+            var stub;
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(options.method, 'GET');
             });
 
             request.get('https://api.glesys.com');
+
+            assert.ok(stub.called);
         });
 
         it('should set the request URL', function () {
             var request = new Request({});
+            var stub;
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(url, 'https://api.glesys.com/api/serviceinfo');
             });
 
             request.get('/api/serviceinfo');
+
+            assert.ok(stub.called);
         });
 
         it('should set the query string', function () {
             var request = new Request({});
+            var stub;
             var data = {
                 foo: 'bar',
             };
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(options.query, data);
             });
 
             request.get('/api/serviceinfo', data);
+
+            assert.ok(stub.called);
         });
     });
 
     describe('post()', function () {
         it('should set the request method to "POST"', function () {
             var request = new Request({});
+            var stub;
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(options.method, 'POST');
             });
 
             request.post('https://api.glesys.com');
+
+            assert.ok(stub.called);
         });
 
         it('should set the request URL', function () {
             var request = new Request({});
+            var stub;
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(url, 'https://api.glesys.com/api/serviceinfo');
             });
 
             request.post('/api/serviceinfo');
+
+            assert.ok(stub.called);
         });
 
         it('should set the body', function () {
             var request = new Request({});
+            var stub;
             var data = {
                 foo: 'bar',
             };
 
-            sinon.stub(request, 'request', function (url, options, callback) {
+            stub = sinon.stub(request, 'request', function (url, options, callback) {
                 assert.strictEqual(options.body, data);
             });
 
             request.post('/api/serviceinfo', data);
+
+            assert.ok(stub.called);
         });
     });
 });
