2399369a484213cbb8e6a36130a0eb4414627df4
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-17 11:12:04 +0200

    Rewrite Customer tests to ES2015

--- test/endpoints/customer.js
+++ test/endpoints/customer.js
@@ -1,41 +1,36 @@
 'use strict';
 
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
 
-var Customer = require('../../lib/endpoints/customer');
-var Request = require('../../lib/request');
+const Customer = require('../../lib/endpoints/customer');
+const Request = require('../../lib/request');
 
-describe('endpoints/customer', function () {
-    describe('contactInfo', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var customer;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+describe('endpoints/customer', () => {
+    describe('contactInfo', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const customer = new Customer(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/customer/contactinfo');
             });
 
-            customer = new Customer(request);
             customer.contactInfo();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var customer;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const customer = new Customer(request);
+            const expected = {
                 companyname: 'companyname',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            customer = new Customer(request);
             customer.contactInfo(expected);
 
             assert.ok(stub.called);
