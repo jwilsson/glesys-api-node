fe321c68f4ebac46a387cb88a810ecb0510d3873
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-17 18:05:55 +0200

    Rewrite User tests to ES2015

--- test/endpoints/user.js
+++ test/endpoints/user.js
@@ -1,215 +1,184 @@
 'use strict';
 
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
 
-var User = require('../../lib/endpoints/user');
-var Request = require('../../lib/request');
+const User = require('../../lib/endpoints/user');
+const Request = require('../../lib/request');
 
-describe('endpoints/user', function () {
-    describe('changePassword', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+describe('endpoints/user', () => {
+    describe('changePassword', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/changepassword');
             });
 
-            user = new User(request);
             user.changePassword();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var user;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const user = new User(request);
+            const expected = {
                 currentpassword: 'currentpassword',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            user = new User(request);
             user.changePassword(expected);
 
             assert.ok(stub.called);
         });
     });
 
-    describe('details', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'get', function (url) {
+    describe('details', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'get', (url) => {
                 assert.strictEqual(url, '/user/details');
             });
 
-            user = new User(request);
             user.details();
 
             assert.ok(stub.called);
         });
     });
 
-    describe('disableTwoFactor', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+    describe('disableTwoFactor', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/disabletwofactor');
             });
 
-            user = new User(request);
             user.disableTwoFactor();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var user;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const user = new User(request);
+            const expected = {
                 password: 'password',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            user = new User(request);
             user.disableTwoFactor(expected);
 
             assert.ok(stub.called);
         });
     });
 
-    describe('edit', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+    describe('edit', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/edit');
             });
 
-            user = new User(request);
             user.edit();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var user;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const user = new User(request);
+            const expected = {
                 name: 'name',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            user = new User(request);
             user.edit(expected);
 
             assert.ok(stub.called);
         });
     });
 
-    describe('enableTwoFactor', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+    describe('enableTwoFactor', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/enabletwofactor');
             });
 
-            user = new User(request);
             user.enableTwoFactor();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var user;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const user = new User(request);
+            const expected = {
                 method: 'method',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            user = new User(request);
             user.enableTwoFactor(expected);
 
             assert.ok(stub.called);
         });
     });
 
-    describe('login', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+    describe('login', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/login');
             });
 
-            user = new User(request);
             user.login();
 
             assert.ok(stub.called);
         });
 
-        it('should set the request body', function () {
-            var request = new Request();
-            var user;
-            var stub;
-            var expected = {
+        it('should set the request body', () => {
+            const request = new Request();
+            const user = new User(request);
+            const expected = {
                 username: 'username',
             };
 
-            stub = sinon.stub(request, 'post', function (url, data) {
+            const stub = sinon.stub(request, 'post', (url, data) => {
                 assert.deepEqual(data, expected);
             });
 
-            user = new User(request);
             user.login(expected);
 
             assert.ok(stub.called);
         });
     });
 
-    describe('logout', function () {
-        it('should set the request URL', function () {
-            var request = new Request();
-            var user;
-            var stub;
-
-            stub = sinon.stub(request, 'post', function (url) {
+    describe('logout', () => {
+        it('should set the request URL', () => {
+            const request = new Request();
+            const user = new User(request);
+            const stub = sinon.stub(request, 'post', (url) => {
                 assert.strictEqual(url, '/user/logout');
             });
 
-            user = new User(request);
             user.logout();
 
             assert.ok(stub.called);
